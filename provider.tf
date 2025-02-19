43d3a84bfe420d5c52c3974d9c86ab57ce1f96e7
Salma Makina <121131192+Salmamakina@users.noreply.github.com>
2025-02-19 16:45:08 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,8 +9,8 @@ terraform {
 
 # Configure the GCP Provider
 provider "google" {
-  credentials = "keplerdata.json"
+  credentials = "keplerdatav1.json"
   project = "keplerdatav1"  # the GCP project ID
   region  = "us-central1"
-  zone    = "us-central1-a"           
+  zone    = "us-central1-b"           
 }
